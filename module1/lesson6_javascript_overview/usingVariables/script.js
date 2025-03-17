273a5ba90312c211d6468481a3337916f9ced4a2
HoangVV01 <hoangvietvu01@gmail.com>
2025-03-17 19:07:33 +0700

    bai tap su dung toan tu

---
+++ module1/lesson6_javascript_overview/usingVariables/script.js
@@ -0,0 +1,11 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt('Enter your input width');
+inputHeight = prompt('Enter your input height');
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+let area = width * height;
+document.write("The area is " + area);
